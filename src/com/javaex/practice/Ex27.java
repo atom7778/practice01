b94f326b881f23f9570db19474e4a129b529a690
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-23 15:38:52 +0900

    Ex 27 답 미정

---
+++ src/com/javaex/practice/Ex27.java
@@ -0,0 +1,18 @@
+package com.javaex.practice;
+/*
+public class Ex27 {
+
+	반지름을 입력받아 원의 면적을 구하는 프로그램을 작성하세요. (파이값은 3.14로 상수로 지정)
+	public static void main(String[] args) {
+		
+		반지름을 입력하세요 : 3
+		원의 넓이는 28.26 
+		
+		
+		답  미정 
+		
+		
+	}
+	
+}
+*/
