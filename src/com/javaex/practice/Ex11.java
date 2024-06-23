0f488f1142e739debd18d6d5d815b3c201d835ea
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-23 15:25:14 +0900

    Ex 11 답 미정

---
+++ src/com/javaex/practice/Ex11.java
@@ -0,0 +1,20 @@
+package com.javaex.practice;
+/*
+public class Ex11 {
+
+	public static void main(String[] args) {
+		
+		int iVar = 10;
+		long Ivar = 1000000000000L;
+		char cVar = 10;
+		float fVar = 12.4;
+		String str = 'ab';
+		
+		답 미정 
+		
+	}
+	
+	
+	
+}
+*/
