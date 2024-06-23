ed7ddfe5011c93f772685f392a0df9a302a9a2e2
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-23 15:38:43 +0900

    Ex 26 답 미정

---
+++ src/com/javaex/practice/Ex26.java
@@ -0,0 +1,18 @@
+package com.javaex.practice;
+/*
+public class Ex26 {
+
+	월급을 입력받아 10년동안 최대 저축액을 계산하는 프로그램을 작성하세요.
+	public static void main(String[] args) {
+		
+		월급을 입력하세요 : 200
+		10년동안 최대 저축액은 24000원 입니다. 
+		
+		답    미정 
+		
+	}
+	
+	
+	
+}
+*/
