a4dc5c70e2ddb17d45166a4507dcd5741d6b5bf5
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-23 15:24:49 +0900

    Ex 08 답 미정

---
+++ src/com/javaex/practice/Ex08.java
@@ -0,0 +1,31 @@
+package com.javaex.practice;
+/*
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		public class hello {
+			
+			public static void main(String[] args) {
+				
+				System.out.println("Hello World");
+				
+				답   
+				
+				
+				
+				
+			}
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
+
+*/
