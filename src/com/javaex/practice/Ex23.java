95b1c2b93aa6a1a46165f41b5aa81450ac6179a9
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-23 15:26:49 +0900

    Ex 23 답 미정

---
+++ src/com/javaex/practice/Ex23.java
@@ -0,0 +1,24 @@
+package com.javaex.practice;
+/*
+public class Ex23 {
+
+	다음의 요구사항을 만족하는 코드를 작성하세요
+	public static void main(String[] args) {
+		
+		요구사항 
+		double  형의 speed,args time, distance 변수를 선언한다.
+		speed 변수에 90.0을 저장한다.
+		time 변수에 60.0을 저장한다.
+		speed와 time을 곱하고 그 결과를 distance에 저장한다.
+		distance를 콘솔에 출력한다.
+		
+		
+		답    미정 
+		
+		
+	}
+	
+	
+	
+}
+*/
