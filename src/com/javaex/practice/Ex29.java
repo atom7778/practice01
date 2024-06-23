2b5b40e784292fcac9ac000957a897cc60c0c217
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-23 15:39:08 +0900

    Ex 29 답 미정

---
+++ src/com/javaex/practice/Ex29.java
@@ -0,0 +1,23 @@
+package com.javaex.practice;
+/*
+public class Ex29 {
+
+	직사각형의 둘레와 면적을 구하는 프로그램을 작성하세요.(가로 세로 변수 double형으로 작성)
+	public static void main(String[] args) {
+		
+		가로를 입력하세요: 3
+		세로를 입력하세요: 4
+		사각형의 넓이는 12.0
+		사각형의 둘레는 14.0 
+		
+		
+		답    미정 
+		
+		
+		
+	}
+	
+	
+	
+}
+*/
