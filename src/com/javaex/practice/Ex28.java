31faae22ed8e223340cdef9cfb02d711f66f10cd
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-23 15:38:59 +0900

    Ex 28 답 미정

---
+++ src/com/javaex/practice/Ex28.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+/*
+public class Ex28 {
+
+	원화를 딜러화로 계산하는 프로그램을 작성하세요.(환율 1달러 = 1230.95원 상수로 지정)
+	public static void main(String[] args) {
+		
+		환전할 원화를 입력하세요: 123095
+		받으실 달러는 100.0
+		
+		
+		답   미정 
+		
+		
+		
+	}
+	
+}
+*/
