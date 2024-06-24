5eb7471454337e1fbc4fc46e695bd2c90b774171
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 02:06:08 +0900

    미작성

--- src/com/javaex/practice/Ex32.java
+++ src/com/javaex/practice/Ex32.java
@@ -1,25 +1,2 @@
-package com.javaex.practice;
 
-import java.util.Scanner;
-
-public class Ex32 {
-
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("가로: ");
-		double w = sc.nextDouble();
-		sc.nextLine();
-		
-		System.out.print("세로: ");
-		double h = sc.nextDouble();
-		sc.nextLine();
-		
-		System.out.println("삼각형의 넓이는 "+w*h/2+" 입니다.");
-		
-		
-		sc.close();
-	}
-}
 	
