4dbafdb9c2d81309dbac37535cb5b8908a8e0a85
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:44:01 +0900

    작성 미완료

--- src/com/javaex/practice/Ex34.java
+++ src/com/javaex/practice/Ex34.java
@@ -1,25 +1,27 @@
 package com.javaex.practice;
-/*
+
+
+import java.util.Scanner;
+
 public class Ex34 {
 	
 	public static void main(String[] args) {
-	Ex18 사용자로부터 화씨 온도를 입력받아 섭씨 온도로 변환하는 프로그램을 작성하세요.
-	
-	c > 섭씨 기호  F = 화씨 기호 
-	화씨 > 섭씨 c = 5/9 * (F - 32)
-	섭씨 > 화씨 F = (C * 9/5) + 32 
-	
-	화씨: 80 
-	화씨 80.0 의 섭씨온도는 26.666666666666668 입니다. 
 		
-	화씨: 200 
-	화씨 200.0 의 섭씨온도는 93.33333333333334 입니다. 
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("화씨: ");
+		
+		double f = sc.nextDouble();
+		
+		sc.nextLine();
+		
+		double c = 5.0/9.0*(f-32.0);
+		
+		
+		System.out.println("화씨 "+f+"의 섭씨온도는 "+c+" 입니다.");
+		
+		sc.close();
 		
 	}
-	
-	
-	
-	
+
 }
-*/
