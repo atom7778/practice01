17168fa8e40146a25524bed62c9157f9de4f09b4
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:43:12 +0900

    작성 미완료

--- src/com/javaex/practice/Ex30.java
+++ src/com/javaex/practice/Ex30.java
@@ -1,21 +1,21 @@
 package com.javaex.practice;
-/*
-public class Ex30 {
 
-	마일을 km로 변환하는 프로그램을 작성하세요.(1마일 = 1.609km 상수로 지정)
+import java.util.Scanner;
+
+public class Ex30 {
+	
 	public static void main(String[] args) {
 		
-		마일을 입력하세요:
-		10.0마일은 16.09킬로미터 입니다.
+		Scanner sc = new Scanner(System.in);
 		
-		마일을 입력하세요:15
-		15.0마일은 24.134999999999998킬로미터 입니다.
-				
+		final double km = 1.609;
 		
-		답   미정 
+		System.out.print("마일을 입력하세요: ");
+		double m = sc.nextDouble();
 		
+		System.out.println(m+"마일은 "+m*km+"킬로미터 입니다.");
+		
+		sc.close();
 	}
-	
-	
+
 }
-*/
