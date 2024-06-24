6c3e1b8d7766ae59d959fc69c93425e08113c34b
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 02:04:17 +0900

    미작성

--- src/com/javaex/practice/Ex38.java
+++ src/com/javaex/practice/Ex38.java
@@ -1,30 +1,3 @@
-package com.javaex.practice;
 
-
-
-import java.util.Scanner;
-
-public class Ex38 {
-	
-	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("전체금액을 입력해주세요: ");
-		
-		int price = sc.nextInt();
-		sc.nextLine();		
-		
-		int amount =(price/1000)*1000;
-		
-		System.out.println("실제지불금액은 "+amount+ " 입니다.");
-		
-		sc.close();
-		
-	}
-}
-
-
-
-
-				
