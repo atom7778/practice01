1ffc563520ca22879fe9d6e8bed9ab702352f913
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:46:01 +0900

    작성 미완료

--- src/com/javaex/practice/Ex23.java
+++ src/com/javaex/practice/Ex23.java
@@ -1,24 +1,16 @@
 package com.javaex.practice;
-/*
-public class Ex23 {
 
-	다음의 요구사항을 만족하는 코드를 작성하세요
+public class Ex23 {
+	
 	public static void main(String[] args) {
 		
-		요구사항 
-		double  형의 speed,args time, distance 변수를 선언한다.
-		speed 변수에 90.0을 저장한다.
-		time 변수에 60.0을 저장한다.
-		speed와 time을 곱하고 그 결과를 distance에 저장한다.
-		distance를 콘솔에 출력한다.
+		double speed, time, distance;
+		speed = 90.0;
+		time = 60.0;
+		distance = speed*time;
 		
-		
-		답    미정 
-		
+		System.out.println(distance);
 		
 	}
-	
-	
-	
+
 }
-*/
