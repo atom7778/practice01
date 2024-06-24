a8549f0f26a0002c78f9414152ab0eff74e1304e
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:43:39 +0900

    작성 미완료

--- src/com/javaex/practice/Ex32.java
+++ src/com/javaex/practice/Ex32.java
@@ -1,29 +1,25 @@
 package com.javaex.practice;
-/*
+
+import java.util.Scanner;
+
 public class Ex32 {
 
 	public static void main(String[] args) {
-	사용자로부터 가로 세로 값을 입력받아 삼각형의 넓이를 구하는 프로그램을 작성하세요
-	
-	가로: 5
-	세로: 10
-	삼각형의 넓이는 25.0 입니다. 
-	
-	가로: 12.5
-	세로: 20
-	삼각형의 넓이는 125.0 입니다. 
-	
-	가로: 22.5
-	세로: 56.4
-	삼각형의 넓이는 634.5 입니다. 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("가로: ");
+		double w = sc.nextDouble();
+		sc.nextLine();
+		
+		System.out.print("세로: ");
+		double h = sc.nextDouble();
+		sc.nextLine();
+		
+		System.out.println("삼각형의 넓이는 "+w*h/2+" 입니다.");
 		
 		
-		
-		
+		sc.close();
 	}
-	
-	
-
-	
 }
-*/
+	
