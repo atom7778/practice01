4ba341c30220c89b95deb4ce12292b10ef2b030c
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:42:51 +0900

    작성 미완료

--- src/com/javaex/practice/Ex28.java
+++ src/com/javaex/practice/Ex28.java
@@ -1,19 +1,23 @@
 package com.javaex.practice;
-/*
+
+import java.util.Scanner;
+
 public class Ex28 {
 
-	원화를 딜러화로 계산하는 프로그램을 작성하세요.(환율 1달러 = 1230.95원 상수로 지정)
 	public static void main(String[] args) {
 		
-		환전할 원화를 입력하세요: 123095
-		받으실 달러는 100.0
+		Scanner sc = new Scanner(System.in);
 		
+		final double d = 1230.95;
 		
-		답   미정 
+		System.out.print("환전할 원화를 입력하세요: ");
+		int w = sc.nextInt();
 		
+		double c = w /d;
 		
+		System.out.println("받으실 달러는 "+c+" 입니다.");
 		
+		sc.close();
 	}
 	
 }
-*/
