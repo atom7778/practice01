2d33aae735e685ff705949dff46489ebe9345738
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:42:40 +0900

    작성 미완료

--- src/com/javaex/practice/Ex27.java
+++ src/com/javaex/practice/Ex27.java
@@ -1,18 +1,22 @@
 package com.javaex.practice;
-/*
-public class Ex27 {
 
-	반지름을 입력받아 원의 면적을 구하는 프로그램을 작성하세요. (파이값은 3.14로 상수로 지정)
+import java.util.Scanner;
+
+public class Ex27 {
+	
 	public static void main(String[] args) {
 		
-		반지름을 입력하세요 : 3
-		원의 넓이는 28.26 
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("반지름을 입력하세요: ");
 		
-		답  미정 
+		int radius = sc.nextInt();
 		
+		double round = radius*radius *3.14;
 		
+		System.out.println("원의 넓이는 "+round+" 입니다.");
+		
+		sc.close();
 	}
-	
+
 }
-*/
