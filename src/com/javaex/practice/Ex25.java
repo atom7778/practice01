75c8903b0121a5c44732c12279ba045e234c70d6
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:42:11 +0900

    답안  도저희 모르겠어요.

--- src/com/javaex/practice/Ex25.java
+++ src/com/javaex/practice/Ex25.java
@@ -1,24 +1,18 @@
 package com.javaex.practice;
-/*
-public class Ex25 {
 
-	아래의 출력결과를 예상하여 작성하세요.
-	(코드를 작성해서 예상과 맞는지 확인해 보세요)
+public class Ex25 {
+	
 	public static void main(String[] args) {
 		
 		int a = 13;
 		
-		System.out.println((a>13) && (a>=13));
-		System.out.println((a>13) || (a>=13));
-		System.out.println((a/3 == 4) && (a% 3== 2));
-		System.out.println((a/3 == 4) || (a% 3== 2));
-		System.out.println((a/3 == 4) && (a% 3== 2) || (a/3 == 4));
-		
-		답 미정 
-		
+		System.out.println((a>13) && (a>=13)); // false
+		System.out.println((a>13) || (a>=13)); // true
+		System.out.println((a/3 == 4) && (a%3 == 2)); // false 1개 틀림
+		System.out.println((a/3 == 4) || (a%3 == 2)); // true 1개만 틀림
+		System.out.println((a/3 == 4) && (a%3 == 2)||(a/3 == 4)); //
+
 		
 	}
-	
-	
+
 }
-*/
