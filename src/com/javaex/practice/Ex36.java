a7cb79e512988e24f5a2f7230550b1c92e8cbe4b
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:44:20 +0900

    작성 미완료

--- src/com/javaex/practice/Ex36.java
+++ src/com/javaex/practice/Ex36.java
@@ -1,29 +1,42 @@
 package com.javaex.practice;
-/*
-public class Ex36 {
 
+import java.util.Scanner;
+
+public class Ex36 {
+	
 	public static void main(String[] args) {
-	동전별 개수를 입력받아 총금액을 계산하는 프로그램을 작성하세요.
-	
-	500원 개수: 1 
-	100원 개수: 1 
-	50원 개수: 1
-	10원 개수: 1
-	동전의 총합은 660 원 입니다. 
 		
+		Scanner sc = new Scanner(System.in);
 		
-	500원 개수: 3
-	100원 개수: 5 
-	50원 개수: 4
-	10원 개수: 3
-	동전의 총합은 2230 원 입니다. 
-	
-	
-	
-	
+		System.out.print("500원 개수: ");
+		int num1 = sc.nextInt();
+		int price1 = num1*500;
+		sc.nextLine();
+				
+		System.out.print("100원 개수: ");
+		
+		int num2 = sc.nextInt();
+		int price2 = num2*100;
+		sc.nextLine();
+		
+		System.out.print("50원 개수: ");
+		
+		int num3 = sc.nextInt();
+		int price3 = num3*50;
+		sc.nextLine();
+		
+		System.out.print("10원 개수: ");
+		
+		int num4 = sc.nextInt();
+		int price4 = num4*10;
+		sc.nextLine();
+		
+		int sum = price1+price2+price3+price4;
+		
+		System.out.println("동전의 총합은 "+sum+"원 입니다.");
+		
+		sc.close();
+		
 	}
-	
-	
-	
+
 }
-*/
