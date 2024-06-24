9f06a37d1905b339be12cc417ab05fb35404125a
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:44:29 +0900

    작성 미완료

--- src/com/javaex/practice/Ex37.java
+++ src/com/javaex/practice/Ex37.java
@@ -1,26 +1,32 @@
 package com.javaex.practice;
-/*
-public class Ex37 {
 
+
+import java.util.Scanner;
+
+public class Ex37 {
+	
 	public static void main(String[] args) {
-	사용자에게 연필의 개수와 인원수를 입력받아 모든인원에게 같은수의 연필을 나눠주려고 한다
-	1인당 연필의 받을수 있는 연필의 개수와 나머지를 구하시오 
-	
-	전체 연필갯수를 입력해주세요: 127
-	전체 인원수를 입력해주세요: 3
-	1인당 연필의 갯수는 42 입니다. 
-	연필의 나머지 갯수는 1 입니다. 
-	
-	전체 연필갯수를 입력해주세요: 45
-	전체 인원수를 입력해주세요: 5
-	1인당 연필의 갯수는 9 입니다. 
-	연필의 나머지 갯수는 0 입니다. 
-	
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("전체 연필갯수를 입력해주세요: ");
+		int p = sc.nextInt();
+		sc.nextLine();		
+		
+		System.out.print("전체 인원수를 입력해주세요: ");
+		int num = sc.nextInt();
+		sc.nextLine();	
+		
+		int oneP = p/num;
+		int r = p%num;
+		
+		
+		System.out.println("1인당 연필의 갯수는 "+oneP+" 입니다.");
+		System.out.println("연필의 나머지 갯수는 "+r+" 입니다.");
+		
+		sc.close();
+	}
+
+}
 			
 		
-	}
-	
-	
-	
-}
-*/
