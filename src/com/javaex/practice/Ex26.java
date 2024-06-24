5d69b0f486e6ffae42446fd8f3f3abbf92f7a78e
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:42:26 +0900

    작성  미완료

--- src/com/javaex/practice/Ex26.java
+++ src/com/javaex/practice/Ex26.java
@@ -1,18 +1,22 @@
 package com.javaex.practice;
-/*
-public class Ex26 {
 
-	월급을 입력받아 10년동안 최대 저축액을 계산하는 프로그램을 작성하세요.
+import java.util.Scanner;
+
+public class Ex26 {
+	
 	public static void main(String[] args) {
 		
-		월급을 입력하세요 : 200
-		10년동안 최대 저축액은 24000원 입니다. 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("월급을 입력하세요: ");
 		
-		답    미정 
+		int pay = sc.nextInt();
 		
+		int maxPay = pay*10*12;
+		
+		System.out.println("10년 동안 최대 저축액은 "+ maxPay +"원 입니다.");
+		
+	
+		sc.close();
 	}
-	
-	
-	
+
 }
-*/
