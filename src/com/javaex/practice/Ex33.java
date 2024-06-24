0c8dbddbbfbf8243c00561587da1216d84517277
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-24 13:01:38 +0900

    Ex33 답 미정

---
+++ src/com/javaex/practice/Ex33.java
@@ -0,0 +1,21 @@
+package com.javaex.practice;
+/*
+public class Ex33 {
+
+	public static void main(String[] args) {
+	사용자로부터 구의 반지름을 입력받아 구의 부피를 계산하는 프로그램을 작성하세요.
+	
+	반지름: 5
+	구의부피는: 523.3333333333334 입니다.
+		
+	반지름: 12
+	구의부피는: 7234.5599999999995 입니다.
+		
+		
+	}
+	
+	
+	
+	
+}
+*/
