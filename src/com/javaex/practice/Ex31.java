082c33cbc843035fc08aba0036469f329288de37
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:43:29 +0900

    작성 미완료

--- src/com/javaex/practice/Ex31.java
+++ src/com/javaex/practice/Ex31.java
@@ -1,28 +1,36 @@
 package com.javaex.practice;
-/*
-public class Ex31 {
 
+import java.util.Scanner;
+
+public class Ex31 {
 	
 	public static void main(String[] args) {
-	상품을 구매하면 정가의 10%를 부가세로 부여한다. 아래와 같이 출력되도록 
-	프로그램을 작성하세요
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("상품가격: ");
+		
+		double price = sc.nextDouble();
+		
+		sc.nextLine();
+		
+		System.out.print("받은 돈: ");
+		
+		double money = sc.nextDouble();
+		
+		sc.nextLine();
+		
+		System.out.println("=================");
+		
+		System.out.println("받은 돈: "+money);
+		System.out.println("상품가격: "+price);
+		System.out.println("부가세: "+price*1/10);
+		System.out.println("잔액: "+ (money-price));
 	
-	상품가격: 7500 
-	받은돈: 10000
-	=============================
-	받은돈: 10000.0
-	상품가격: 7500.0
-	부가세: 750.0
-	잔액: 2500.0
-
 		
-		
-		
-		
+		sc.close();
+				
 	}
-	
-	
-	
+
 }
-*/
 
