a5d358d664865bbc5c955a637ad653b65fffa0af
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 02:09:19 +0900

    미작성

--- src/com/javaex/practice/Ex17.java
+++ src/com/javaex/practice/Ex17.java
@@ -1,21 +1,2 @@
-package com.javaex.practice;
 
-public class Ex17 {
-	
-	public static void main(String[] args) {
-		
-		int x,y;
-		
-		x = 10;
-		y = 20;
-		
-		int sum = x+y;
-		
-		System.out.println("합은" + sum);
-		
-
-	}
-
-
-}
 	
