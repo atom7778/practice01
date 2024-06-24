c7f68b04c956cd9770e78330b3fdb09f504b5fb3
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:43:02 +0900

    작성 미완료

--- src/com/javaex/practice/Ex29.java
+++ src/com/javaex/practice/Ex29.java
@@ -1,23 +1,27 @@
 package com.javaex.practice;
-/*
-public class Ex29 {
 
-	직사각형의 둘레와 면적을 구하는 프로그램을 작성하세요.(가로 세로 변수 double형으로 작성)
+import java.util.Scanner;
+
+public class Ex29 {
+	
 	public static void main(String[] args) {
 		
-		가로를 입력하세요: 3
-		세로를 입력하세요: 4
-		사각형의 넓이는 12.0
-		사각형의 둘레는 14.0 
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("가로를 입력하세요: ");
+		double width = sc.nextDouble();
+		sc.nextLine();
 		
-		답    미정 
+		System.out.print("세로를 입력하세요: ");
+		double height = sc.nextDouble();
+		sc.nextLine();
 		
+		System.out.println("사각형의 넓이는 "+width*height);
+		System.out.println("사각형의 둘레는 "+(width+height)*2);
 		
+		sc.close();
 		
 	}
-	
-	
+
 	
 }
-*/
