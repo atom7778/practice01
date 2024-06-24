92169fc61bbc404769e378435ec93870943af7b4
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-25 01:46:59 +0900

    작성 미완료

--- src/com/javaex/practice/Ex21.java
+++ src/com/javaex/practice/Ex21.java
@@ -1,22 +1,15 @@
 package com.javaex.practice;
-/*
-public class Ex21 {
 
-	아래의 출력결과를 예상하여 작성하세요. Ex20문제와 어느 부분이 다른지 비교해 보세요
-	(코드를 작성해서 예상과 맞는지 확인해 보세요.)
+public class Ex21 {
+	
 	public static void main(String[] args) {
 		
 		int i = 10;
 		int n = ++i %2;
 		
-		System.out.println(i);
-		System.out.println(n);
-		
-		답   미정 
-		
+		System.out.println(i); // 11
+		System.out.println(n); // 1
 		
 	}
-	
-	
+
 }
-*/
