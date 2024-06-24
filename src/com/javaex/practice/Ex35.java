bc2167c315065d73d6663198c31f7c9aa8bb760e
knr98 <knr98@DESKTOP-CJ9DN18>
2024-06-24 13:01:57 +0900

    Ex35 답 미정

---
+++ src/com/javaex/practice/Ex35.java
@@ -0,0 +1,18 @@
+package com.javaex.practice;
+/*
+public class Ex35 {
+
+	public static void main(String[] args) {
+	long형 변수를 사용하여 빛이 1년동안 진행하는 거리를 계산하여 출력하세요.
+	(단 빛의 속도는 300000 km/s 로 계산한다)
+	
+	빛이 1년 동안가는 거리는 9460800000000 km 입니다. 
+		
+		
+	}
+	
+	
+	
+	
+}
+*/
